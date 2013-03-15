ea234c2b1b74e1b61adfc81ace32b2c1bed45792
Boris Sanchez <nook.p3g105@gmail.com>
2013-03-15 15:02:45 -0500

    small print mods

--- TokenSearch/StdPrintResultsSearch.cpp
+++ TokenSearch/StdPrintResultsSearch.cpp
@@ -15,11 +15,11 @@ void StdPrintResultsSearch::print(Search* search){
     const std::list<LineResult*> lineResults = search->getLineResults();;
     for(std::list<LineResult*>::const_iterator citl = lineResults.begin(); citl != lineResults.end(); citl++){
         const std::list<TokenResult*> tokenResults = (*citl)->getTokenResults();
-        std::cout<<"LINE NUMBER: "<<(*citl)->getLineNumber()<<std::endl;
+        std::cout<<"> LINE NUMBER: "<<(*citl)->getLineNumber()<<std::endl;
         for (std::list<TokenResult*>::const_iterator citt = tokenResults.begin(); citt != tokenResults.end(); citt++) {
             char* result = NULL;
             (*citt)->getResult(&result);
-            std::cout<<"TOKEN index{"<<(*citt)->getIndex()<<"} = "<<result<<std::endl;
+            std::cout<<"--- TOKEN index{"<<(*citt)->getIndex()<<"} = "<<result<<std::endl;
             if(result!=NULL){
                 delete[] result;
             }
