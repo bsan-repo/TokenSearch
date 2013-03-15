1dff6b8984731b4c0bb299fb6fe0ae98149eb4de
Boris Sanchez <nook.p3g105@gmail.com>
2013-03-15 13:04:31 -0500

    Functionality to print the results of Search

---
+++ TokenSearch/StdPrintResultsSearch.cpp
@@ -0,0 +1,28 @@
+//
+//  StdPrintResultsSearch.cpp
+//  TokenSearch
+//
+//  Created by b san on 3/15/13.
+//  Copyright (c) 2013 b san. All rights reserved.
+//
+
+#include "StdPrintResultsSearch.h"
+#include <list>
+#include "LineResult.h"
+#include "TokenResult.h"
+
+void StdPrintResultsSearch::print(Search* search){
+    const std::list<LineResult*> lineResults = search->getLineResults();;
+    for(std::list<LineResult*>::const_iterator citl = lineResults.begin(); citl != lineResults.end(); citl++){
+        const std::list<TokenResult*> tokenResults = (*citl)->getTokenResults();
+        std::cout<<"LINE NUMBER: "<<(*citl)->getLineNumber()<<std::endl;
+        for (std::list<TokenResult*>::const_iterator citt = tokenResults.begin(); citt != tokenResults.end(); citt++) {
+            char* result = NULL;
+            (*citt)->getResult(&result);
+            std::cout<<"TOKEN index{"<<(*citt)->getIndex()<<"} = "<<result<<std::endl;
+            if(result!=NULL){
+                delete[] result;
+            }
+        }
+    }
+}
---
+++ TokenSearch/StdPrintResultsSearch.h
@@ -0,0 +1,20 @@
+//
+//  StdPrintResultsSearch.h
+//  TokenSearch
+//
+//  Created by b san on 3/15/13.
+//  Copyright (c) 2013 b san. All rights reserved.
+//
+
+#ifndef __TokenSearch__StdPrintResultsSearch__
+#define __TokenSearch__StdPrintResultsSearch__
+
+#include <iostream>
+#include "Search.h"
+
+class StdPrintResultsSearch{
+public:
+    static void print(Search* search);
+};
+
+#endif /* defined(__TokenSearch__StdPrintResultsSearch__) */
