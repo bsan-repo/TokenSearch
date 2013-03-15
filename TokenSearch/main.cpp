9f3ab67d1a5dd93debb1fabd98c92e55809b5080
Boris Sanchez <nook.p3g105@gmail.com>
2013-03-15 15:02:16 -0500

    Corrected to accept the file from stdin and the pattern from the arguments

--- TokenSearch/main.cpp
+++ TokenSearch/main.cpp
@@ -17,41 +17,31 @@
 
 int main(int argc, const char * argv[])
 {
+    // insert code here...
+    std::cout << "Token search utility - v1\n\n";
+    std::cout << "End of line with 1 = Found, 0 = Not found\n\n";
+    
     char *line = new char[1000];
-    char* defaultPattern = {(char*)"bar %{0G} foo %{1}"};//"foo %{0} is a %{1S0}"};// DEV
     char patternStr[200];
     
     // TODO argument verification
     if(argc > 1){
         strcpy(patternStr, argv[1]);
-    }else{
-        strcpy(patternStr, defaultPattern);
+    
+        File* file = new File();
+        Pattern* pattern = new PCREPattern(patternStr);
+        PCREProcessor processor;
+        Search searchInLines(file, pattern, &processor);
         
-    }
-    
-    // insert code here...
-    std::cout << "Token search utility - v1\n\n";
-    
-    
-    
-    File* file = new File();
-    Pattern* pattern = new PCREPattern(patternStr);
-    PCREProcessor processor;
-    Search searchInLines(file, pattern, &processor);
-    
-    std::cout << "Pattern: "<<patternStr<<std::endl;
-    char* regEx = NULL;
-    pattern->getRegEx(&regEx);
-    std::cout << "RegEx: "<<regEx<<std::endl;
-    
-    FILE *testFile;
-    if((testFile = fopen("/Users/6opC4h4e3/Desktop/testFile.txt", "r"))== NULL){
-        printf("file not found\n");
-    }else{
+        // Print info
+        std::cout << "Pattern: "<<patternStr<<std::endl;
+        char* regEx = NULL;
+        pattern->getRegEx(&regEx);
+        std::cout << "RegEx: "<<regEx<<std::endl;
+        
         int dataCounter = 0;
-        while(!feof(testFile)){
-            char c = fgetc(testFile);
-            //printf("%c", c);
+        while(!feof(stdin)){
+            char c = fgetc(stdin);
             if(c == '\n'){
                 line[dataCounter] = NULL;
                 dataCounter = 0;
@@ -66,35 +56,14 @@ int main(int argc, const char * argv[])
         dataCounter = 0;
         file->addLine(line);
         printf("> %s  : %d \n\n", line, searchInLines.isPatternInLine(line));
+        printf("---------------------\n");
+    
+        Search search(file, pattern, &processor);
+        std::cout<<"\n\n--------------------- TOKEN RESULS -----------------\n";
+        StdPrintResultsSearch::print(&search);
+    }else{
+        std::cout<<"No pattern was given.\n";
     }
-    
-    Search search(file, pattern, &processor);
-    std::cout<<"\n\n--------------------- TOKEN RESULS -----------------\n";
-    StdPrintResultsSearch::print(&search);
-    
-    
-    /*
-    printf("---------------------\n");
-    int dataCounter = 0;
-    while(!feof(stdin)){
-        char c = fgetc(stdin);
-        printf("%c", c);
-        if(c == '\n'){
-            line[dataCounter] = NULL;
-            dataCounter = 0;
-            file->addLine(line);
-        }else{
-            line[dataCounter] = c;
-            dataCounter++;
-        }
-     }
-     line[dataCounter-1] = NULL;
-     dataCounter = 0;
-     file->addLine(line);
-    printf("---------------------\n");
-    */
-    
-    delete file;
     
     return 0;
 }
